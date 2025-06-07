16669f2817ac24f0fe9051f5dc055211a885c5eb
Maria-AAU <NH33TU@student.aau.dk>
2025-06-07 10:36:38 +0200

    Update partition_function_strauss.R
    funk

---
+++ R/partition_function_strauss.R
@@ -0,0 +1,22 @@
+funk <- function(n, beta, gamma, r){
+  sum = 0
+  for (i in 1:n){
+    X = rpoispp(1, owin())
+    antal = npoints(X)
+    if (antal > 1){
+      count = 0
+      for (k in 1:antal){
+        for (j in 1:antal){
+    if (i != j){
+      if (sqrt((X$x[i]-X$x[j])^2-(X$y[i]-X$y[j])^2)<=r){
+      count = count+1
+      
+      }
+    }
+        }
+      }
+    }
+    sum = sum+beta^(antal)*gamma^(count)
+  } 
+  return(sum/n)
+}
