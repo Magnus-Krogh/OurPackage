c64dc0b9cb8740e76fb740d44dfc2dc19384a575
henriettebjrn <55382597+henriettebjrn@users.noreply.github.com>
2025-06-07 11:46:41 +0200

    Update quadrat_split.R

--- R/quadrat_split.R
+++ R/quadrat_split.R
@@ -1,4 +1,4 @@
 quadrat_patterns <- lapply(patterns, function(pp) {
-        quads <- quadrats(pp, nx = 3, ny = 3)   # splitter i 3 x 3 vindue
+        quads <- quadrats(pp, nx = 3, ny = 3)
         split.ppp(pp, f = quads)
       })
