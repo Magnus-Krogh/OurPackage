80ba3edf7af295f869570e89c1fb665d9f8e1d24
Magnus Krogh <magnus.n.krogh@gmail.com>
2025-06-07 10:35:01 +0200

    two functions

---
+++ R/yule_walker_est.R
@@ -0,0 +1,25 @@
+yule_walker_est <- function(x){
+  number_of_params <- length(x) - 1
+  ps <- c(x[2]/x[1])
+  v <- c(x[1]*(1-ps[1]^2))
+  if (number_of_params > 1){
+    for (i in 2:number_of_params){
+      temp_phis <- rep(0, i-1)
+      sum <- 0
+      for (j in 1:(i-1)){
+        sum <- sum + ps[j] * x[i - j + 1]
+      }
+      phi_p <- (x[i + 1] - sum) / v
+      for (j in 1:(i-1)){
+        temp_phis[j] <- ps[j] - phi_p * ps[i - j]
+      }
+      ps <- c(temp_phis, phi_p)
+      v <- v*(1 - ps[i]^2)
+    }
+  }
+  sum <- 0
+  for (i in 1:(number_of_params)){
+    sum <- sum + ps[i]*x[i+1]
+  }
+  return(c(ps, x[1] - sum))
+}
